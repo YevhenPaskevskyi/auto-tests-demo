0cd7a6c2cc63491f283f0d765bb6426048085393
YevhenPaskevskyi <itntarget@gmail.com>
2025-12-19 23:49:03 +0100

    Steps of the tests

--- tests/ui/02_login.test.js
+++ tests/ui/02_login.test.js
@@ -1,10 +1,21 @@
 import { test, expect } from '@playwright/test';
+import { allure } from 'allure-playwright';
 
-test('successful login on saucedemo.com', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.fill('#user-name', 'standard_user');
-  await page.fill('#password', 'secret_sauce');
-  await page.click('#login-button');
+test('successful login', async ({ page }) => {
+  await allure.step('Open login page', async () => {
+    await page.goto('/');
+  });
 
-  await expect(page.locator('.inventory_list')).toBeVisible();
+  await allure.step('Enter credentials', async () => {
+    await page.fill('#user-name', 'standard_user');
+    await page.fill('#password', 'secret_sauce');
+  });
+
+  await allure.step('Click login', async () => {
+    await page.click('#login-button');
+  });
+
+  await allure.step('Verify successful login', async () => {
+    await expect(page).toHaveURL(/inventory/);
+  });
 });
