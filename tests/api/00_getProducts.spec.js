968eeddea5097f4b8a1fc9ae6b645c2654cf2ab5
YevhenPaskevskyi <itntarget@gmail.com>
2025-12-20 01:53:12 +0100

    Adding steps to API tests

--- tests/api/00_getProducts.spec.js
+++ tests/api/00_getProducts.spec.js
@@ -1,8 +1,10 @@
 import { test, expect } from '@playwright/test';
+import * as allure from "allure-js-commons";
 test.skip(process.env.CI === 'true', 'Skip API tests in CI');
 
 
 test('GET /products returns list of products', async ({ request }) => {
+  await allure.step('Get products', async () => {
   const response = await request.get('https://fakestoreapi.com/products');
 
   expect(response.status()).toBe(200);
@@ -11,4 +13,5 @@ test('GET /products returns list of products', async ({ request }) => {
 
   expect(Array.isArray(products)).toBe(true);
   expect(products.length).toBeGreaterThan(0);
+});
 });
