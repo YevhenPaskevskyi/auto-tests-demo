682abc5a8ccc201371b7a9875834df1da3aa1f77
YevhenPaskevskyi <itntarget@gmail.com>
2026-01-08 16:47:25 +0100

    One file for Products CRUD fix2

--- tests/api/specs/Products.spec.js
+++ tests/api/specs/Products.spec.js
@@ -1,4 +1,6 @@
 import { test, expect } from '../../fixtures/apiFixtures.js';
+test.skip(process.env.CI === 'true', 'Skip API tests in CI');
+
 
 test('Products flow: get list -> get by id(from list) -> create -> update -> delete', async ({ productsApi }) => {
   let existingId;
@@ -10,7 +12,7 @@ test('Products flow: get list -> get by id(from list) -> create -> update -> del
     const status = res.status();
     const url = res.url();
     const headers = res.headers();
-    const bodyText = await res.text(); // читаем 1 раз
+    const bodyText = await res.text(); 
   
     console.log('GET /products URL:', url);
     console.log('GET /products STATUS:', status);
